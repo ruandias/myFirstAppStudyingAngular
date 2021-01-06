e3b7659c84e69d6a569f244ccf9d45ba84c55b99
ruandias <ruan_ndias@hotmail.com>
2021-01-05 21:33:02 -0300

    created home/breweries route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { BreweriesComponent } from './breweries/breweries.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    BreweriesComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/breweries/breweries.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-breweries',
+  templateUrl: './breweries.component.html',
+  styleUrls: ['./breweries.component.scss']
+})
+export class BreweriesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
